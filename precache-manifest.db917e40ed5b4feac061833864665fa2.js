076958f2145fc93815247672bd0ca7f2436363a1
Ahmad Muslih Zain <aemzayn@gmail.com>
2020-06-19 10:55:33 +0300

    Updates

---
+++ precache-manifest.db917e40ed5b4feac061833864665fa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528707b4052de64f050dd10489e99cff",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "3a3677f36353fe56c330",
+    "url": "/quiz-app/static/css/main.a9773844.chunk.css"
+  },
+  {
+    "revision": "3ab71f2d961c8c4d526d",
+    "url": "/quiz-app/static/js/2.336ff80c.chunk.js"
+  },
+  {
+    "revision": "12ac8af0741afe107c3dcb4ef10e1ce5",
+    "url": "/quiz-app/static/js/2.336ff80c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3677f36353fe56c330",
+    "url": "/quiz-app/static/js/main.3a3654d0.chunk.js"
+  },
+  {
+    "revision": "06c84507df5e70193f3c",
+    "url": "/quiz-app/static/js/runtime-main.c542d00b.js"
+  }
+]);
